55f30661b65212b132e10e1eb24efe728335a646
Olivier de Broqueville <olivier.debroqueville@gmail.com>
2023-05-07 21:42:24 +0200

    Original adaptation in French

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,9 +9,9 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+const QA_PROMPT = `You are an expert lawyer in French. Use the following pieces of context to answer the question at the end. The abreviation "Art." stands for "Article" in French and is usually followed by the number of the article.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context. Always answer in French.
 
 {context}
 
@@ -19,19 +19,19 @@ Question: {question}
 Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
-  const model = new OpenAI({
-    temperature: 0, // increase temepreature to get more creative answers
-    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
-  });
+    const model = new OpenAI({
+        temperature: 0, // increase temepreature to get more creative answers
+        modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access otherwise use gpt-3.5-turbo
+    });
 
-  const chain = ConversationalRetrievalQAChain.fromLLM(
-    model,
-    vectorstore.asRetriever(),
-    {
-      qaTemplate: QA_PROMPT,
-      questionGeneratorTemplate: CONDENSE_PROMPT,
-      returnSourceDocuments: true, //The number of source documents returned is 4 by default
-    },
-  );
-  return chain;
+    const chain = ConversationalRetrievalQAChain.fromLLM(
+        model,
+        vectorstore.asRetriever(),
+        {
+            qaTemplate: QA_PROMPT,
+            questionGeneratorTemplate: CONDENSE_PROMPT,
+            returnSourceDocuments: true, //The number of source documents returned is 4 by default
+        },
+    );
+    return chain;
 };
